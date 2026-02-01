641d24f757f6b7c182141902805d662cc1ad39fd
AlvaroWarrior <guerrerogonzalez.alvaro@gmail.com>
2026-02-01 01:24:35 +0100

    Mapa

--- app/buscar/page.tsx
+++ app/buscar/page.tsx
@@ -4,7 +4,7 @@ export default function BuscarPage() {
   return (
     <div className="mx-auto max-w-6xl px-6 py-6">
       <h1 className="text-2xl font-semibold mb-4">
-        Buscar cerca de mí
+        Buscar cerca de mí!
       </h1>
 
       <MapNearMe />
